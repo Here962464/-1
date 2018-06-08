a87cc4fb5b01fd3e4caf78ebb91c8e365fa35c98
crazyJiaLin <crazy_JiaLin@163.com>
2018-06-08 21:34:45 +0800

    授权页设计

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -47,16 +47,16 @@ Page({
         if (res.authSetting['scope.userInfo']) {
           // 已经授权
           // 率先加载用户界面
-          wx.navigateTo({
-            url: 'home/home'
+          wx.switchTab({
+            url: '../home/home'
           })
           //调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function(res){
-              console.log(res)
-              _this.loadUserInfo(res)
-            }
-          })
+          // wx.getUserInfo({
+          //   success: function(res){
+          //     console.log(res)
+          //     _this.loadUserInfo(res)
+          //   }
+          // })
         }else{
          console.log("用户未授权");
         }
@@ -65,9 +65,20 @@ Page({
   },
   // 唤起请求获取用户信息弹窗
   getUserInfo: function (e) {
-    console.log(e.detail.userInfo)
+    console.log(e)
+    wx.getUserInfo({
+      success: function(e){
+        console.log(e)
+      },
+      fail: function(e){
+
+      }
+    })
     var _this = this;
-    this.loadUserInfo(e.detail);
+    // this.loadUserInfo(e.detail);
+    wx.switchTab({
+      url: '../home/home'
+    })
   },
   loadUserInfo: function(data){
     var _this = this;
